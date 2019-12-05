3598d67248081a01a1e8be37314d734fe933397a
Daniel <teemu.daniel.laajala@gmail.com>
2019-12-05 15:54:02 +0200

    Testing matrix operations in Fortran

--- src/blassof.f95
+++ src/blassof.f95
@@ -4,6 +4,7 @@ module blasso
     implicit none
     private
     public :: testblasso
+    public :: blassomat
  
 contains
  
@@ -21,4 +22,32 @@ contains
  
     end subroutine testblasso
  
+    subroutine blassomat(x, nrow, ncol, res) bind(C, name = "blassomat_f_")
+ 
+        integer(kind = c_int), intent(in), value		:: nrow 	!Number of rows in x
+        integer(kind = c_int), intent(in), value		:: ncol 	!Number of cols in x
+        real(kind = c_double), intent(in), dimension(nrow*ncol) :: x    	!Vector of values
+        real(kind = c_double), intent(out), dimension(ncol)     :: res  	!Output variable
+        integer                                         	:: i,j,ind	!Internal count
+	real(kind = c_double), dimension(nrow,ncol)		:: xmat 	!Matrix inside Fortran 
+ 
+        ind = 0
+        DO i = 1, nrow
+          DO j = 1, ncol
+            ind = ind +1
+            xmat(i,j) = x(ind)
+          END DO
+        END DO
+
+	! Compute column averages as an example of a matrix operation
+        res = 0.0_c_double
+	DO j=1, ncol
+	  DO i=1, nrow
+	    res(j) = res(j) + xmat(i,j)
+	  END DO
+	  res(j) = res(j) / nrow
+	END DO
+ 
+    end subroutine blassomat
+  
 end module blasso
