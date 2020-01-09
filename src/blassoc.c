74a3091dc4221aa7a6355cfa9ef3e856e2d9cb4b
Syksy <teemu.daniel.laajala@gmail.com>
2020-01-09 18:50:20 +0200

    Constructing Fortran cox budget-aware fitting interface

--- src/blassoc.c
+++ src/blassoc.c
@@ -8,6 +8,8 @@
 void F77_NAME(testblasso_f)(double *x, int n, double *res);
 // Matrix testing
 void F77_NAME(blassomat_f)(double *x, int nrow, int ncol, double *res);
+// Actual fitting function for budget-aware LASSO (Cox regression)
+void F77_NAME(blassocoxfit_f(int *yevent, int *ytime, double *x, int xnrow, int xncol, int *k, int knrow, int kncol, double *costvec, double *beta);
 
 // Define the C wrapper function that calls Fortran
 extern SEXP c_testblasso_f(SEXP x){
@@ -37,10 +39,36 @@ extern SEXP c_blassomat_f(SEXP x, SEXP nrow, SEXP ncol){
 	return(res);
 }
 
+// Define the C wrapper function for budget-aware LASSO
+extern SEXP c_blassocoxfit_f(SEXP yevent, SEXP ytime, SEXP x, SEXP xnrow, SEXP xncol, SEXP k, SEXP knrow, SEXP kncol, SEXP costvec){
+	/Rprintf("step1 \n");
+	// Integers for x-matrix dimensions
+	const int xnr = asInteger(xnrow);
+	const int xnc = asInteger(xncol);
+	// Integers for k-matrix dimensions
+	const int knr = asInteger(knrow);
+	const int knc = asInteger(kncol);
+
+	Rprintf("xnrow: %i , kncol: %i \n", xnr, xnc);
+	Rprintf("knrow: %i , kncol: %i \n", knr, knc);
+	SEXP beta;
+	//SEXP cols = PROTECT(allocVector(INTSXP, 1
+	//Rprintf("step2 \n");
+	// The new vector of coefficients beta is as long as the number of columns in data matrix x
+	PROTECT(beta = allocVector(REALSXP, xnc));
+	/Rprintf("step3 \n");
+	F77_CALL(blassofit_f)(INTEGER(yevent), INTEGER(ytime), REAL(x), xnr, xnc, INTEGER(k), knr, knx, REAL(costvec), REAL(beta));
+	/Rprintf("step4 \n");
+	UNPROTECT(1);
+	/Rprintf("step5 \n");
+	return(beta);
+}
+
 // Tell R of our available Fortran functions; should probably have one for each model family
 static const R_CallMethodDef CallEntries[] = {
-  {"c_testblasso_f",	(DL_FUNC) &c_testblasso_f,	1}, // count of input arguments
-  {"c_blassomat_f",	(DL_FUNC) &c_blassomat_f,		1}, // count of input arguments
+  {"c_testblasso_f",	(DL_FUNC) &c_testblasso_f,	1},
+  {"c_blassomat_f",	(DL_FUNC) &c_blassomat_f,		1},
+  {"c_blassocoxfit_f", (DL_FUNC) &c_blassocoxfit_f,	1},
   {NULL,				NULL,						0}
 };
 
--- src/blassof.f95
+++ src/blassof.f95
@@ -5,6 +5,7 @@ module blasso
     private
     public :: testblasso
     public :: blassomat
+    public :: blassocoxfit
  
 contains
  
@@ -59,5 +60,71 @@ contains
   	write(*,*) 'End of Fortran subroutine'
 
     end subroutine blassomat
+
+ 
+    ! Subroutine placeholder for actual budget-aware LASSO
+    subroutine blassocoxfit(yevent, ytime, x, xnrow, xncol, k, knrow, kncol, costvec, beta) bind(C, name = "blassocoxfit_f_")
+
+	! Data input (response and data)
+	integer(kind = c_int), intent(in), dimension(xnrow)		:: yevent	!Integer of death event (1) or censoring (0)
+	integer(kind = c_int), intent(in), dimension(xnrow)		:: ytime	!Integer of follow-up times (presumably in full days/months/etc)
+        real(kind = c_double), intent(in), dimension(xnrow*xncol)	:: x    	!Vector of data values
+        integer(kind = c_int), intent(in), dimension(knrow*kncol) 	:: k    	!Vector of indicator matrix values
+        real(kind = c_double), intent(in), dimension(xncol)		:: costvec    	!Vector of data values
+	! Helper variables, i.e. populating x and k matrices
+        integer(kind = c_int), intent(in), value			:: xnrow 	!Number of rows in data matrix X
+        integer(kind = c_int), intent(in), value			:: xncol 	!Number of cols in data matrix X
+        integer(kind = c_int), intent(in), value			:: knrow 	!Number of rows in kit indicator matrix K
+        integer(kind = c_int), intent(in), value			:: kncol 	!Number of cols in kit indicator matrix K
+	! Variables for internal use in Fortran
+	real(kind = c_double), dimension(xnrow,xncol)			:: xmat 	!Data matrix inside Fortran 
+	integer(kind = c_int), dimension(knrow,kncol)			:: kmat		!Indicator matrix inside Fortran 
+        integer                                         		:: i,j,ind	!Internal variables
+	! Output back to C and R
+        real(kind = c_double), intent(out), dimension(ncol)     	:: beta  	!Output variable
+ 
+ 	write(*,*) 'Function starts'
+ 	!write(*,*) nrow, ncol
+ 	!write(*,*) x
+ 
+ 	! Populate the matrix xmat
+        ind = 0
+	DO j = 1, xncol
+          DO i = 1, xnrow
+            ind = ind +1
+            xmat(i,j) = x(ind)
+          END DO
+        END DO
+	! Populate the indicator matrix kmat
+	ind = 0
+	DO j = 1, kncol
+          DO i = 1, knrow
+            ind = ind +1
+            kmat(i,j) = k(ind)
+          END DO
+        END DO
+	
+
+ 	write(*,*) 'Matrix populated'
+
+	!!!!
+	!
+	! Kaisa's algorithm procedures here!
+	! Ought to produce suitable beta fit based on input variables
+	!
+	!!!!
+
+	! Compute column averages as an example of a matrix operation (here beta would be the fitted model coefficients)
+        beta = 0.0_c_double
+	DO j=1, xncol
+	  DO i=1, xnrow
+	    beta(j) = beta(j) + xmat(i,j)
+	  END DO
+	  beta(j) = beta(j) / xnrow
+	END DO
+ 
+  	write(*,*) 'End of Fortran subroutine'
+
+    end subroutine blassocoxfit
   
 end module blasso
