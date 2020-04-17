271b1263ada65970d32db3acf9982ae435369ddc
Syksy <teemu.daniel.laajala@gmail.com>
2020-04-17 13:06:50 +0300

    Sanity checking and testing output changes

--- src/blassof.f95
+++ src/blassof.f95
@@ -26,7 +26,7 @@ contains
         real(kind = c_double), intent(in), dimension(nkits)	:: costs	!Costs associated to each kit
         ! OUTPUT
         real(kind = c_double), intent(out), dimension(ncol)     :: beta  	!Output variable for beta coefficients
-        real(kind = c_double), intent(out), dimension(ncol*nkits):: fperk  	!Output variable target function value per k
+        real(kind = c_double), intent(out), dimension(1)	:: fperk  	!Output variable target function value per k
 	!
 	!!!
 
@@ -34,7 +34,8 @@ contains
         !
         ! Local variables
         !
-        integer                                         	:: i,j,ind	! Internal count
+        integer(kind = c_int)					:: i,j,ind	! Internal count
+        integer(kind = c_int)					:: nr, nc, nk	! Number of rows and columns of X, and number of kits
 	real(kind = c_double), dimension(nrow,ncol)		:: xmat 	! Data matrix
 	integer(kind = c_int), dimension(nrow,2)		:: ymat		! Survival response matrix/vector
  	integer(kind = c_int), dimension(nkits,ncol)		:: kmat		! Kit indicator matrix
@@ -48,10 +49,14 @@ contains
   	write(*,*) y
   	write(*,*) kits
   	
+  	! Set the number of rows and columns inside Fortran
+  	nr = nrow
+  	nc = ncol
+  	nk = nkits
  	! Populate the matrix xmat of dim {nrow,ncol}
         ind = 0
-	DO j = 1, ncol
-          DO i = 1, nrow
+	DO j = 1, nc
+          DO i = 1, nr
             ind = ind +1
             xmat(i,j) = x(ind)
           END DO
@@ -59,22 +64,22 @@ contains
         ! Populate response matrix of dim {nrow,2}
 	ind = 0
 	DO j = 1, 2
-	  DO i = 1, nrow
+	  DO i = 1, nr
 	    ind = ind + 1
 	    ymat(i,j) = y(ind)
 	  END DO
 	END DO
         ! Populate response matrix of dim {nkits,ncol}
 	ind = 0
-	DO j = 1, ncol
-	  DO i = 1, nkits
+	DO j = 1, nc
+	  DO i = 1, nk
 	    ind = ind + 1
 	    kmat(i,j) = kits(ind)
 	  END DO
 	END DO
 	! Populate the cost vector for kits
 	ind = 0
-	DO i = 1, nkits
+	DO i = 1, nk
 	  ind = ind + 1
 	  cvec(i) = costs(ind)
 	END DO
@@ -88,14 +93,26 @@ contains
  	write(*,*) kmat
  	write(*,*) 'cvec:'
  	write(*,*) cvec
-
+	
+	write(*,*) 'Values of nr, nc, and nk:'
+	write(*,*) nr
+	write(*,*) nc
+	write(*,*) nk
+	
+	write(*,*) 'First row of xmat:'
+	write(*,*) xmat(1,:)
+	
 	! Format variables for coefficients, function value per k, etc
-        beta = 1.0_c_double ! Test non-zero double formatting
+        beta = 1.234_c_double ! Test non-zero double formatting
         fperk = 0.0_c_double
 
 	! 
 	! ... ACTUAL ALGORITHM ...
 	!
+ 
+ 	! Example modifying based on the algorithm... 	
+ 	beta(1) = 1.23
+	! Works but risky?!
  
   	write(*,*) 'End of Fortran subroutine'
 
