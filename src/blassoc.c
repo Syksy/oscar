f583e92314a2f778b912384d030a220bd3925b5b
nabuliini <58519381+nabuliini@users.noreply.github.com>
2020-08-06 17:31:34 +0300

    Update blassoc.c

--- src/blassoc.c
+++ src/blassoc.c
@@ -5,7 +5,10 @@
 #include <R_ext/Rdynload.h>
 
 // Blasso Cox DBDC
-void F77_NAME(cassocox_f)(double *x, double *y, int *kits, double* cvec, int nrow, int ncol, int nkits, double *beta, double *fperk, int print, int start);
+void F77_NAME(casso_cox_f)(double *x, double *y, int *kits, double* cvec, int nrow, int ncol, int nkits, double *beta, double *fperk, int print, int start);
+void F77_NAME(casso_mse_f)(double *x, double *y, int *kits, double* cvec, int nrow, int ncol, int nkits, double *beta, double *fperk, int print, int start);
+void F77_NAME(casso_logistic_f)(double *x, int *y, int *kits, double* cvec, int nrow, int ncol, int nkits, double *beta, double *fperk, int print, int start);
+
 
 // Define the C wrapper function for matrix operation testing
 extern SEXP c_cassocox_f(SEXP x, SEXP y, SEXP kits, SEXP cvec, SEXP nrow, SEXP ncol, SEXP nkits, SEXP print, SEXP start){
@@ -22,7 +25,67 @@ extern SEXP c_cassocox_f(SEXP x, SEXP y, SEXP kits, SEXP cvec, SEXP nrow, SEXP n
 	PROTECT(fperk = allocVector(REALSXP, nk));
 
 	// Call Fortran subroutine
-	F77_CALL(cassocox_f)(REAL(x), REAL(y), INTEGER(kits), REAL(cvec), nr, nc, nk, REAL(beta), REAL(fperk),inprint, instart);
+	F77_CALL(casso_cox_f)(REAL(x), REAL(y), INTEGER(kits), REAL(cvec), nr, nc, nk, REAL(beta), REAL(fperk),inprint, instart);
+
+	// Create result structure
+	SEXP res = PROTECT(allocVector(VECSXP,2));
+	SET_VECTOR_ELT(res,0,beta);
+	SET_VECTOR_ELT(res,1,fperk);
+
+	// Wrap up and return
+	UNPROTECT(1);
+	UNPROTECT(1);
+	UNPROTECT(1);
+
+	//return(beta);
+	return(res);
+}
+// Wrapper for mean square error solver
+extern SEXP c_cassomse_f(SEXP x, SEXP y, SEXP kits, SEXP cvec, SEXP nrow, SEXP ncol, SEXP nkits, SEXP print, SEXP start){
+	// Define constants (dimensions in data / features)
+	const int nr = asInteger(nrow);
+	const int nc = asInteger(ncol);
+	const int nk = asInteger(nkits);
+	const int inprint = asInteger(print);
+	const int instart = asInteger(start);
+	SEXP beta;
+	SEXP fperk;
+	// Format output and protect them from garbage collection
+	PROTECT(beta = allocVector(REALSXP, nc*nk));
+	PROTECT(fperk = allocVector(REALSXP, nk));
+
+	// Call Fortran subroutine
+	F77_CALL(casso_mse_f)(REAL(x), REAL(y), INTEGER(kits), REAL(cvec), nr, nc, nk, REAL(beta), REAL(fperk),inprint, instart);
+
+	// Create result structure
+	SEXP res = PROTECT(allocVector(VECSXP,2));
+	SET_VECTOR_ELT(res,0,beta);
+	SET_VECTOR_ELT(res,1,fperk);
+
+	// Wrap up and return
+	UNPROTECT(1);
+	UNPROTECT(1);
+	UNPROTECT(1);
+
+	//return(beta);
+	return(res);
+}
+// Wrapper for logistic solver
+extern SEXP c_cassologistic_f(SEXP x, SEXP y, SEXP kits, SEXP cvec, SEXP nrow, SEXP ncol, SEXP nkits, SEXP print, SEXP start){
+	// Define constants (dimensions in data / features)
+	const int nr = asInteger(nrow);
+	const int nc = asInteger(ncol);
+	const int nk = asInteger(nkits);
+	const int inprint = asInteger(print);
+	const int instart = asInteger(start);
+	SEXP beta;
+	SEXP fperk;
+	// Format output and protect them from garbage collection
+	PROTECT(beta = allocVector(REALSXP, nc*nk));
+	PROTECT(fperk = allocVector(REALSXP, nk));
+
+	// Call Fortran subroutine
+	F77_CALL(casso_logistic_f)(REAL(x), INTEGER(y), INTEGER(kits), REAL(cvec), nr, nc, nk, REAL(beta), REAL(fperk),inprint, instart);
 
 	// Create result structure
 	SEXP res = PROTECT(allocVector(VECSXP,2));
@@ -40,6 +103,8 @@ extern SEXP c_cassocox_f(SEXP x, SEXP y, SEXP kits, SEXP cvec, SEXP nrow, SEXP n
 // Tell R of our available Fortran functions; should probably have one for each model family
 static const R_CallMethodDef CallEntries[] = {
   {"c_cassocox_f",	(DL_FUNC) &c_cassocox_f,		9},
+  {"c_cassomse_f",	(DL_FUNC) &c_cassomse_f,		9},
+  {"c_cassologistic_f",	(DL_FUNC) &c_cassologistic_f,		9},
   {NULL,				NULL,						0}
 };
 
