755ad2334bf9567a6897edcebd128ad20c1178ea
Syksy <teemu.daniel.laajala@gmail.com>
2023-03-28 18:58:44 +0300

    Fixed UTF-8 issue in one of the references

--- R/pckg.R
+++ R/pckg.R
@@ -8,7 +8,7 @@
 #'
 #' OSCAR models utilize the L0-pseudonorm to select an optimal subset of features that generalizes linear regression models to a variety of families. Currently supported models include conventional Gaussian regression (family="mse" or family="gaussian"), Binomial/Logistic regression (family="logistic"), and Cox proportional hazards modeling (family="cox").
 #' 
-#' @references Halkola AS, Joki K, Mirtti T, M‰kel‰ MM, Aittokallio T, Laajala TD (2023) OSCAR: Optimal subset cardinality regression using the L0-pseudonorm with applications to prognostic modelling of prostate cancer. PLoS Comput Biol 19(3): e1010333. \doi{10.1371/journal.pcbi.1010333}
+#' @references Halkola AS, Joki K, Mirtti T, M√§kel√§ MM, Aittokallio T, Laajala TD (2023) OSCAR: Optimal subset cardinality regression using the L0-pseudonorm with applications to prognostic modelling of prostate cancer. PLoS Comput Biol 19(3): e1010333. \doi{10.1371/journal.pcbi.1010333}
 #'
 #' @docType package
 #' @name oscar-package
